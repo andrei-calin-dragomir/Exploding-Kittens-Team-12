e26941d43e107fea2b6051f844085645f9607d41
Sander <v.sanderamsterdam@gmail.com>
2021-03-28 04:35:57 +0200

    Added offline settings partial screen, and server connect working screen.

--- src/main/java/softwaredesign/gui/OfflineSettingsController.java
+++ src/main/java/softwaredesign/gui/OfflineSettingsController.java
@@ -1,37 +1,42 @@
 package softwaredesign.gui;
 
-import javafx.animation.AnimationTimer;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Shape;
-import softwaredesign.client.ClientProgram;
 
 import java.io.File;
-import java.lang.reflect.Array;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.EventObject;
 import java.util.ResourceBundle;
-import java.util.concurrent.TimeUnit;
 
 public class OfflineSettingsController implements Initializable {
 
     @FXML
-    private Label playerAmount;
+    private Label playerAmount, deckSelection;
 
     @FXML
-    private Shape startGame, leftButtonPlayers, rightButtonPlayers;
+    private Shape startGame, leftButtonPlayers, rightButtonPlayers, leftButtonDeck, rightButtonDeck;
 
     ScrollButton playerAmountScroll = new ScrollButton();
+    ScrollButton deckSelectionScroll = new ScrollButton();
 
     @FXML
     void rotatePlayerAmount(MouseEvent event){
         playerAmountScroll.navigate((Shape) event.getSource());
+    }
+
+    @FXML
+    void rotateDeckAmount(MouseEvent event){
+        deckSelectionScroll.navigate((Shape) event.getSource());
+    }
+
+    @FXML
+    void startGame(){
+        Integer amountOfPlayers = Integer.parseInt(playerAmount.getText());
+        String deckToUse = deckSelection.getText();
+        // Offline mode has been deleted wtf? I can't seem to find it anymore. Anyway just pass these arguments and start offline.
     }
 
     @FXML
@@ -43,10 +48,12 @@ public class OfflineSettingsController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         playerAmountScroll.initButtons(leftButtonPlayers, rightButtonPlayers, playerAmount);
-        getDeckNames();
+        deckSelectionScroll.initButtons(leftButtonDeck, rightButtonDeck, deckSelection);
+        playerAmountScroll.addText(new String[]{"1", "2", "3", "4"});
+        deckSelectionScroll.addText(getDeckNames());
     }
 
-    public ArrayList<String> getDeckNames(){
+    public String[] getDeckNames(){
         ArrayList<String> allDecks = new ArrayList<>();
         File folder = new File("resources/decks");
         File[] allFiles = folder.listFiles();
@@ -54,7 +61,7 @@ public class OfflineSettingsController implements Initializable {
         for (int i = 0; i < allFiles.length; i++)
             if (allFiles[i].isFile())
                 allDecks.add(allFiles[i].getName().split("\\.")[0]);
-        playerAmountScroll.addText(allDecks.toArray(new String[allDecks.size()]));
-        return allDecks;
+
+        return allDecks.toArray(new String[allDecks.size()]);
     }
 }
